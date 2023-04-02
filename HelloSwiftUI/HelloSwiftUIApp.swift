e1b926e378e58131fa754f90adaf1baae64a6383
Yuki Naka <48172941+Yuki-Naka-Sharing-Study@users.noreply.github.com>
2023-04-02 20:39:38 +0900

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by 仲優樹 on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
